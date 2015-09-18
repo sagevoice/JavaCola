55b18387b5cef15dcb1be50ddfa6bef896a8babb
sagevoice <jks@sagevoice.com>
2015-09-18 15:34:27 -0700

    refactor out the Poly code

--- src/test/java/edu/monash/infotech/marvl/cola/Tests.java
+++ src/test/java/edu/monash/infotech/marvl/cola/Tests.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.MapType;
 import edu.monash.infotech.marvl.cola.geom.Geom;
 import edu.monash.infotech.marvl.cola.geom.Point;
+import edu.monash.infotech.marvl.cola.geom.TangentVisibilityGraph;
 import edu.monash.infotech.marvl.cola.powergraph.Configuration;
 import edu.monash.infotech.marvl.cola.powergraph.LinkTypeAccessor;
 import edu.monash.infotech.marvl.cola.powergraph.Module;
@@ -301,89 +302,6 @@ public class Tests {
         }, valueHolder);
     }
 
-    private double length(final Point p, final Point q) {
-        final double dx = p.x - q.x, dy = p.y - q.y;
-        return dx * dx + dy * dy;
-    }
-
-    private List<Point> makePoly(final PseudoRandom rand) {
-        return makePoly(rand, 10, 10);
-    }
-
-    private List<Point> makePoly(final PseudoRandom rand, final int width, final int height) {
-        final int n = nextInt(rand, 7) + 3;
-        List<Point> P = new ArrayList<>();
-        loop:
-        for (int i = 0; i < n; ++i) {
-            Point p = new Point(nextInt(rand, width), nextInt(rand, height));
-            int ctr = 0;
-            while (0 < i && 1 > length(P.get(i - 1), p) // min segment length is 1
-                   || 1 < i && ( // new point must keep poly convex
-                    0 >= Geom.isLeft(P.get(i - 2), P.get(i - 1), p)
-                    || 0 >= Geom.isLeft(P.get(i - 1), p, P.get(0))
-                    || 0 >= Geom.isLeft(p, P.get(0), P.get(1)))) {
-                if (10 < ctr++) {
-                    break loop; // give up after ten tries (maybe not enough space left for another convex point)
-                }
-                p = new Point(nextInt(rand, width), nextInt(rand, height));
-            }
-            P.add(p);
-        }
-        if (2 < P.size()) { // must be at least triangular
-            P.add(new Point(P.get(0).x, P.get(0).y));
-            return P;
-        }
-        return makePoly(rand, width, height);
-    }
-
-    private List<List<Point>> makeNonoverlappingPolys(final PseudoRandom rand, final int n) {
-        List<List<Point>> P = new ArrayList<>();
-        Predicate<List<Point>> overlaps = (p) -> {
-            for (int i = 0; i < P.size(); i++) {
-                final List<Point> q = P.get(i);
-                if (Geom.polysOverlap(p, q)) { return true; }
-            }
-            return false;
-        };
-        for (int i = 0; i < n; i++) {
-            List<Point> p = makePoly(rand);
-            while (overlaps.test(p)) {
-                double dx = nextInt(rand, 10) - 5, dy = nextInt(rand, 10) - 5;
-                p.forEach((pt) -> { pt.x += dx; pt.y += dy; });
-            }
-            P.add(p);
-        }
-        List<Point> minPoly = new ArrayList<>();
-        minPoly.add(new Point(Double.MAX_VALUE, Double.MAX_VALUE));
-        minPoly = P.stream().reduce(minPoly, (poly0, poly1) -> {
-            final Point minPt1 = poly1.stream()
-                                      .reduce(new Point(Double.valueOf(Double.MAX_VALUE), Double.valueOf(Double.MAX_VALUE)), (pt0, pt1) -> {
-                                          pt0.x = Math.min(pt0.x, pt1.x);
-                                          pt0.y = Math.min(pt0.y, pt1.y);
-                                          return pt0;
-                                      });
-            final Point minPt0 = poly0.get(0);
-            minPt0.x = Math.min(minPt0.x, minPt1.x);
-            minPt0.y = Math.min(minPt0.y, minPt1.y);
-            return poly0;
-        });
-        final double minX = minPoly.get(0).x, minY = minPoly.get(0).y;
-        P.forEach((p) -> {
-            p.forEach((pt) -> { pt.x -= minX; pt.y -= minY; });
-        });
-        return P;
-    }
-
-    private Point midPoint(final List<Point> p) {
-        double mx = 0, my = 0;
-        final int n = p.size() - 1;
-        for (int i = 0; i < n; i++) {
-            final Point q = p.get(i);
-            mx += q.x;
-            my += q.y;
-        }
-        return new Point(mx / n, my / n);
-    }
 
     private int countRouteIntersections(final List<List<Segment>> routes) {
         final List<Point> ints = new ArrayList<>();
@@ -686,16 +604,15 @@ public class Tests {
         Assert.assertTrue(true);
     }
 
-  /*
+   /*
     @Test(description="tangent visibility graph")
     public void tangentVisibilityGraphTest() {
-        for (var tt = 0; tt < 100; tt++) {
+        for (int tt = 0; tt < 100; tt++) {
             PseudoRandom rand = new PseudoRandom(tt);
-                nextInt = function (r) { return Math.round(rand.getNext() * r) },
-                n = 10,
-                P = makeNonoverlappingPolys(rand, n),
-                port1 = midPoint(P[8]),
-                port2 = midPoint(P[9]);
+                int n = 10;
+                List<List<Point>> P = Poly.makeNonoverlappingPolys(rand, n);
+                Point port1 = Poly.midPoint(P.get(8));
+                Point port2 = Poly.midPoint(P.get(9));
                         TangentVisibilityGraph g = new TangentVisibilityGraph(P);
                 start = g.addPoint(port1, 8),
                 end = g.addPoint(port2, 9);
